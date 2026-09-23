8f1d7b55e78793389fbf3cade28a1d5102e77507
Utsav Kumar <kutsav972@gmail.com>
2026-09-23 18:54:50 +0530

    Implement Complex class with addition and display methods
    Fixed syntax errors and improved the Complex class implementation.

--- ipu_mandatory/complex.cpp
+++ ipu_mandatory/complex.cpp
@@ -1,1 +1,32 @@
-
+#include<iostream> 
+class Complex{ 
+float real; 
+float imag; 
+public: 
+Complex(){ 
+real = 0; 
+imag = 0; 
+} 
+Complex (float r, float i){ 
+Real = r; 
+imag = I; 
+} 
+Complex add(Complex c){ 
+Complex  temp; 
+temp.real = real + c.real; 
+temp.imag = imag + c.imag; 
+retrun temp; 
+} 
+void show(){ 
+std::cout<<real<<” i “<<imag<<std::endl; 
+} 
+}; 
+int main(){ 
+Complex c1; 
+c1.show(); 
+Complex c2.(3,4); 
+Complex c3(2,7); 
+Complex c4= c2.add(c3) 
+c4.show(); 
+return 0; 
+}
